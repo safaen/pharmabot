41e00adb42355963a90a849617386df94e32aed0
Safae Nyan <131413651+safaen@users.noreply.github.com>
2026-04-22 02:20:05 +0200

    Update watchdog.py

--- src/pharmabot/pharmabot/watchdog.py
+++ src/pharmabot/pharmabot/watchdog.py
@@ -1,18 +1,17 @@
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import json,time
+import json, time
 
 class Watchdog(Node):
     def __init__(self):
         super().__init__('watchdog')
-        self.sub=self.create_subscription(String,'scheduled_tasks',self.check,10)
+        self.sub = self.create_subscription(String,'scheduled_tasks',self.check,10)
 
     def check(self,msg):
         t=json.loads(msg.data)
         if time.time()>t["deadline"]:
-            self.get_logger().error("DEADLINE MISSED")
+            self.get_logger().error("🚨 DEADLINE MISSED")
 
 def main():
     rclpy.init()
