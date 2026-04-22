5c0ec041f3f087c628a8e80ea7092d271b0ff4b2
Safae Nyan <131413651+safaen@users.noreply.github.com>
2026-04-22 02:18:18 +0200

    Update robot_nav.py

--- src/pharmabot/pharmabot/robot_nav.py
+++ src/pharmabot/pharmabot/robot_nav.py
@@ -1,4 +1,3 @@
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -6,28 +5,47 @@ from geometry_msgs.msg import PoseStamped
 import json
 
 class RobotNav(Node):
+
     def __init__(self):
         super().__init__('robot_nav')
-        self.sub = self.create_subscription(String,'scheduled_tasks',self.navigate,10)
-        self.pub = self.create_publisher(PoseStamped,'/goal_pose',10)
 
-    def navigate(self,msg):
-        task=json.loads(msg.data)
+        self.sub = self.create_subscription(
+            String,
+            'scheduled_tasks',
+            self.navigate,
+            10
+        )
+
+        self.pub = self.create_publisher(
+            PoseStamped,
+            '/goal_pose',
+            10
+        )
+
+    def navigate(self, msg):
+        task = json.loads(msg.data)
+
         locations = {
-            "ICU": (6.0,2.0),
-            "Pharmacy": (-6.0,2.0),
-            "RoomA": (2.0,-2.0)
+            "ICU": (6.0, 2.0),
+            "Pharmacy": (-6.0, 2.0),
+            "RoomA": (2.0, -2.0)
         }
-        x,y = locations.get(task["location"],(0.0,0.0))
+
+        x, y = locations.get(task["location"], (0.0, 0.0))
+
         goal = PoseStamped()
-        goal.header.frame_id="map"
-        goal.pose.position.x=x
-        goal.pose.position.y=y
-        goal.pose.orientation.w=1.0
+        goal.header.frame_id = "map"
+
+        goal.pose.position.x = x
+        goal.pose.position.y = y
+        goal.pose.orientation.w = 1.0
+
         self.pub.publish(goal)
+
+        self.get_logger().info(f"🚀 Robot → {task['location']} ({x},{y})")
 
 def main():
     rclpy.init()
-    node=RobotNav()
+    node = RobotNav()
     rclpy.spin(node)
     rclpy.shutdown()
